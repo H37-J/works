259a1534943f456744185d40280d875c3db4a429
hjk <these990712@gmail.com>
2024-08-22 23:00:46 +0900

    update

--- pages/api/create.ts
+++ pages/api/create.ts
@@ -1,31 +1,38 @@
-import { PrismaClient } from '@prisma/client'
-import type { NextApiRequest, NextApiResponse } from "next";
+import {PrismaClient} from '@prisma/client'
+import type {NextApiRequest, NextApiResponse} from "next";
 
 const prisma = new PrismaClient()
 
-export default async function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+type Data = {
+    name: string;
+    region: string;
+    phone: string;
+    content: string;
+};
+
+export default async function handler(req: NextApiRequest,
+                                      res: NextApiResponse<Data>,) {
     console.log(req)
     console.log(req.method)
     if (req.method === 'POST') {
         try {
-            const { name, region, phone, content } = req.body
+            const {name, region, phone, content} = req.body
             const customer = await prisma.customer.create({
                 data: {
                     name,
                     region,
+                    phone,
                     content
                 },
             })
             res.status(200).json(customer)
         } catch (error) {
-            res.status(500).json({ error: 'Fail insert' })
+            res.status(500).json({error: 'Fail insert'})
         } finally {
             await prisma.$disconnect()
         }
     } else {
+        console.log('uuuuu')
         // res.setHeader('Allow', ['POST'])
         // res.status(405).end(`Method ${req.method} Not Allowed`)
     }
