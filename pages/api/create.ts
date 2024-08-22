91670554e55e71daddbc28d3c1d71c11c272cbf4
hjk <these990712@gmail.com>
2024-08-22 22:26:26 +0900

    update

--- pages/api/create.ts
+++ pages/api/create.ts
@@ -25,7 +25,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             await prisma.$disconnect()
         }
     } else {
-        res.setHeader('Allow', ['POST'])
-        res.status(405).end(`Method ${req.method} Not Allowed`)
+        // res.setHeader('Allow', ['POST'])
+        // res.status(405).end(`Method ${req.method} Not Allowed`)
     }
 }
