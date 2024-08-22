a46086ba66d93453b521cfee33d707e4b9041607
hjk <these990712@gmail.com>
2024-08-22 22:12:28 +0900

    update

--- pages/api/createData.ts
+++ pages/api/createData.ts
@@ -11,7 +11,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 data: {
                     name,
                     region,
-                    phone,
                     content
                 },
             })
